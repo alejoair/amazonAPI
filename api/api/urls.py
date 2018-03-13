e2b15cd072aeb99889c0604cf50f63e0676742ad
alejo <alejandro.cuartas@yahoo.com>
2018-03-12 20:36:50 -0500

    api creado

--- api/api/urls.py
+++ api/api/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apirest.views import APIview
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', APIview)
 ]
---
+++ api/apirest/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+
+
+import requests as req
+import bs4 
+import lxml
+import json
+import re
+# Create your views here.
+
+def APIview(request):
+
+	query = request.GET.get("query") #OBTIENE EL QUERY
+	asin = ""
+	if query:
+		asin = query
+	else:
+		asin = B07BC6DPBV # ASIN POR DEFECTO
+
+	headers = {'Cache-Control': 'no-cache', 'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
+	urlbase = "https://www.amazon.com/dp/"
+	urlfull = urlbase+asin
+
+	#---------------------------------------------------
+	#----------Hace el Request-----------------------------------------
+	respuesta = req.get(urlfull,headers=headers)
+	soup = bs4.BeautifulSoup(respuesta.content,"lxml")
+
+	#-------------------------------------------------
+	# Obtiene el Titulo
+	titulotag = soup.title.text
+	titulo = titulotag.replace("Amazon.com: ","")
+	titulolimpio = titulo.replace(": Clothing","")
+
+	#Obtiene BSR----------------------------------
+	#---------------------------------------------
+	bsrtag = soup.find(id="SalesRank")
+	if bsrtag:
+		bsr = bsrtag.contents[2]
+	else:
+		bsr = "Sin Ranking"
+
+	#OBTENER BULLETS--------------------------------
+	#-----------------------------------------------
+
+	bulletstag = soup.find(id="featurebullets_feature_div")	
+	bullet1 = bulletstag.contents[1].find_all("li")[4].text
+	bullet1 = bullet1.replace("\n","")
+	bullet1 = bullet1.replace("\t","")
+	bullet2 = bulletstag.contents[1].find_all("li")[5].text
+	bullet2 = bullet2.replace("\n","")
+	bullet2 = bullet2.replace("\t","")
+
+	#-----------------------------------------------------
+	#-----Obtener Precio
+
+	preciotag = soup.find(id="priceblock_ourprice")
+	precio = preciotag.text
+
+	# Devuelve el archivo JSON--------------------
+	#---------------------------------------------
+	return JsonResponse({"titulo": titulolimpio,"bsr": bsr,"precio":precio,"bullet 1":bullet1,"bullet 2":bullet2}, safe=False)
