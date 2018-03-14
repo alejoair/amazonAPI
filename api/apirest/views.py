cf9df26d7c218e50fba926dc4412b08dad3b80b5
alejo <alejandro.cuartas@yahoo.com>
2018-03-14 14:09:54 -0500

    no se

--- api/apirest/views.py
+++ api/apirest/views.py
@@ -13,6 +13,7 @@ import re
 def APIview(request):
 
 	query = request.GET.get("asin") #OBTIENE EL QUERY
+	urlimg = request.GET.get("urlimg") #OBTIENE EL QUERY
 	asin = ""
 	if query:
 		asin = query
@@ -24,11 +25,16 @@ def APIview(request):
 	headers = {'Cache-Control': 'no-cache', 'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
 	urlbase = "https://www.amazon.com/dp/"
 	urlfull = urlbase+asin
-
+	respuesta = ""
 	#---------------------------------------------------
 	#----------Hace el Request-----------------------------------------
-	respuesta = req.get(urlfull,headers=headers)
+	try:
+		respuesta = req.get(urlfull,headers=headers)
+	except:
+		return JsonResponse({"ERROR":"ERROR 500"},safe= False)
+
 	if respuesta.status_code != 200:
+		print("ERROR 500")
 		return JsonResponse({"ERROR":"ERROR 500"},safe= False)
 	soup = bs4.BeautifulSoup(respuesta.content,"lxml")
 
@@ -37,6 +43,10 @@ def APIview(request):
 	titulotag = soup.title.text
 	titulo = titulotag.replace("Amazon.com: ","")
 	titulolimpio = titulo.replace(": Clothing","")
+
+	# Obtiene URL----------------------------
+	#-------------------------------------------
+	url = "https://www.amazon.com/dp/"+asin
 
 	#Obtiene BSR----------------------------------
 	#---------------------------------------------
@@ -65,12 +75,19 @@ def APIview(request):
 	elif leng == 7 :
 		bullet1 = lista[3].text #7
 		bullet2 = lista[5].text
+	elif leng == 5 :
+		bullet1 = lista[3].text #7
+		bullet2 = lista[4].text
 	elif leng == 4 :
 		bullet1 = lista[3].text #4
 		bullet2 = "Sin descripcion"
 	else:	
 		bullet1 = lista[3].text #otros
-		bullet2 = lista[4].text
+		try:
+			bullet2 = lista[4].text
+		except:
+			bullet2 = "Sin descripcion"
+
 	bullet1 = bullet1.replace("\n","")
 	bullet1 = bullet1.replace("\t","")
 	
@@ -85,4 +102,4 @@ def APIview(request):
 
 	# Devuelve el archivo JSON--------------------
 	#---------------------------------------------
-	return JsonResponse({"titulo": titulolimpio,"bsr": bsr,"precio":precio,"bullet 1":bullet1,"bullet 2":bullet2}, safe=False)
+	return JsonResponse({"titulo": titulolimpio,"bsr": bsr,"precio":precio,"bullet1":bullet1,"bullet2":bullet2,"urlimg":urlimg,"url":url}, safe=False)
